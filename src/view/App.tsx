1dd16909667ad829096654e723fa84b414bf6971
Cameron Martin <cameronmartin123@gmail.com>
2019-11-20 23:30:26 +0000

    Make the board not validate moves.

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,9 +1,19 @@
 import Board from './Board';
-import React, { useState } from 'react';
-import { GoGame } from '../model/GoGame';
+import React, { useState, useCallback } from 'react';
+import { GoGame, GoMove } from '../model/GoGame';
 
 export default function App() {
   const [game, setGame] = useState(GoGame.create(9));
 
-  return <Board game={game} setGame={setGame} />;
+  const playMove = useCallback(
+    (getMove: (game: GoGame) => GoMove) => {
+      setGame(game => {
+        const move = getMove(game);
+        return game.validateMove(move) == null ? game.playMove(move) : game;
+      });
+    },
+    [setGame],
+  );
+
+  return <Board game={game} playMove={playMove} />;
 }
