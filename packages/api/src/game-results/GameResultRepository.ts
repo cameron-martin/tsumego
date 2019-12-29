57e301cf113b67badabd0c4c4d820b9ee7661743
Cameron Martin <cameron.martin@forgerock.com>
2019-12-29 21:08:13 +0000

    Use pool query shorthand.

--- packages/api/src/game-results/GameResultRepository.ts
+++ packages/api/src/game-results/GameResultRepository.ts
@@ -1,6 +1,5 @@
-import { GameResult } from './GameResult';
 import { Pool } from 'pg';
-import { withClient } from '../common/data-access';
+import { GameResult } from './GameResult';
 
 export class GameResultRepository {
   constructor(private readonly pool: Pool) {}
@@ -8,16 +7,14 @@ export class GameResultRepository {
   async create(gameResult: GameResult): Promise<void> {
     const userWon = gameResult.result === 'correct';
 
-    await withClient(this.pool, client =>
-      client.query({
-        text: `INSERT INTO game_results (puzzle_id, user_id, user_won, played_at) VALUES ($1, $2, $3, $4)`,
-        values: [
-          gameResult.puzzleId,
-          gameResult.userId,
-          userWon,
-          gameResult.playedAt,
-        ],
-      }),
-    );
+    await this.pool.query({
+      text: `INSERT INTO game_results (puzzle_id, user_id, user_won, played_at) VALUES ($1, $2, $3, $4)`,
+      values: [
+        gameResult.puzzleId,
+        gameResult.userId,
+        userWon,
+        gameResult.playedAt,
+      ],
+    });
   }
 }
--- packages/api/src/puzzle/PuzzleRepository.ts
+++ packages/api/src/puzzle/PuzzleRepository.ts
@@ -1,20 +1,17 @@
 import { Pool, QueryConfig } from 'pg';
 import { Puzzle } from './Puzzle';
 import { WithId, withId } from '../WithId';
-import { withClient } from '../common/data-access';
 
 export default class PuzzleRepository {
   constructor(private readonly pool: Pool) {}
 
-  create(puzzle: Puzzle): Promise<number> {
-    return withClient(this.pool, async client => {
-      const res = await client.query(
-        'INSERT INTO puzzles(puzzle) VALUES ($1) RETURNING id',
-        [JSON.stringify(puzzle.spec)],
-      );
+  async create(puzzle: Puzzle): Promise<number> {
+    const res = await this.pool.query(
+      'INSERT INTO puzzles(puzzle) VALUES ($1) RETURNING id',
+      [JSON.stringify(puzzle.spec)],
+    );
 
-      return res.rows[0].id;
-    });
+    return res.rows[0].id;
   }
 
   get(id: number): Promise<WithId<Puzzle> | null> {
@@ -30,17 +27,15 @@ export default class PuzzleRepository {
     });
   }
 
-  private queryOne(config: QueryConfig): Promise<WithId<Puzzle> | null> {
-    return withClient(this.pool, async client => {
-      const res = await client.query(config);
+  private async queryOne(config: QueryConfig): Promise<WithId<Puzzle> | null> {
+    const res = await this.pool.query(config);
 
-      if (res.rows.length === 0) {
-        return null;
-      }
+    if (res.rows.length === 0) {
+      return null;
+    }
 
-      const row = res.rows[0];
+    const row = res.rows[0];
 
-      return withId(row.id, Puzzle.create(row.puzzle));
-    });
+    return withId(row.id, Puzzle.create(row.puzzle));
   }
 }
