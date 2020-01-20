1ee874c90ef1344744aa336393477e56f9bb86ee
Cameron Martin <cameronmartin123@gmail.com>
2020-01-20 20:01:32 +0000

    Make play redirect to home if user is not authenticated.

--- packages/ui/pages/play.tsx
+++ packages/ui/pages/play.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Router from 'next/router';
 import StandardTemplate from '../src/view/StandardTemplate';
 import Puzzle from '../src/view/Puzzle';
 import { useApiClient } from '../src/apiClient';
 import Loading from '../src/view/Loading';
+import { useAuth } from '../src/view/auth/AuthProvider';
 
 export default function Index() {
   const apiClient = useApiClient();
+  const authState = useAuth();
+
+  useEffect(() => {
+    if (authState && !authState.userId) {
+      Router.replace('/');
+    }
+  }, [authState]);
 
   return (
     <StandardTemplate>
-      {apiClient == null ? <Loading /> : <Puzzle apiClient={apiClient} />}
+      {apiClient == null || authState == null || authState.userId == null ? (
+        <Loading />
+      ) : (
+        <Puzzle apiClient={apiClient} />
+      )}
     </StandardTemplate>
   );
 }
